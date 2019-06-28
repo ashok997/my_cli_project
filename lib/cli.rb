631e32f92fcd357b8bf7f07fa09729cc1dbd9b91
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-28 02:25:23 +0000

    added basic structure to cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,28 @@
 class CLI
   def run
-    puts 'hello from cli'
+    self.welcome
+    self.list_players
   end
+
+  def welcome
+    puts "Welcome fans of Manchester United!\n\n"
+  end
+
+  def list_players(players_hash)
+    
+    # Players {[Goalkeepers:][Defenders:][Midfielders:][Forwards:]}
+    
+  end
+  
+  def player_info(player) #Player has first_name,last_name,position,dob,date_joined,club_debut,bio
+    puts player.first_name
+    puts player.last_name
+    puts player.position
+    puts player.dob
+    puts player.date_joined
+    puts player.club_debut
+    puts "\n\n"
+    puts player.bio
+  end
+
 end
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 require_relative "./my_cli_project/version"
+
 
 module MyCliProject
   class Error < StandardError; end
